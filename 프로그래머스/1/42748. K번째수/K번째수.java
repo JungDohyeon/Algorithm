8ce74f7de99cbcfd87bca2c9e27bac3a83db1980
Jung Dohyeon (Lavine) <jdh1109ok@Naver.com>
2024-04-10 23:04:53 +0900

    [level 1] Title: K번째수, Time: 0.37 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -10,11 +10,50 @@ class Solution {
             int[] copyArr = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
             
             // 배열 정렬
-            Arrays.sort(copyArr);
+            quickSort(copyArr, 0, copyArr.length-1);
+            
+            for(int k= 0; k < copyArr.length; k++) {
+                System.out.print(copyArr[k]);
+            }
+            System.out.println();
             
             answer[i] = copyArr[commands[i][2] - 1];
         }
 
         return answer;
+    }
+    
+    private void quickSort(int[] arr, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        
+        int mid = partition(arr, left, right);
+        quickSort(arr, left, mid - 1);
+        quickSort(arr, mid, right);
+    }
+    
+    private int partition(int[] arr, int left, int right) {
+        int pivot = arr[left];
+        
+        while (left <= right) { 
+            while(arr[left] < pivot)
+                left++;
+            
+            while(arr[right] > pivot) 
+                right--;
+            
+            if (left <= right) { 
+                swap(arr, left++, right--);
+            }
+        }
+    
+        return left;
+    }
+    
+    private void swap(int[] arr, int a, int b) { 
+        int tmp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = tmp;
     }
 }
