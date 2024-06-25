7d6915ff59f3b03e09f3b1a9a209578a829e62af
Jung Dohyeon (Lavine) <jdh1109ok@naver.com>
2024-06-26 03:02:52 +0900

    [Silver I] Title: 정수 삼각형, Time: 240 ms, Memory: 24220 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int[][] triangle;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine());
+        triangle = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            for (int j = 0; j < i + 1; j++) {
+                triangle[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int i = 1; i < N; i++) {
+            for (int j = 0; j < i + 1; j++) {
+                if (j == 0) {
+                    triangle[i][j] += triangle[i - 1][j];
+                } else if (j == i) {
+                    triangle[i][j] += triangle[i - 1][j - 1];
+                } else {
+                    triangle[i][j] += Math.max(triangle[i - 1][j - 1], triangle[i - 1][j]);
+                }
+            }
+        }
+
+        int answer = 0;
+
+        for (int value: triangle[N - 1]) {
+            answer = Math.max(value, answer);
+        }
+
+        bw.write(String.valueOf(answer));
+        bw.flush();
+        bw.close();
+    }
+}
